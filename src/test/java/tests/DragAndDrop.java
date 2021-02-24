7e75e3bee92b7aac25b3278ca580d696df655f7a
roidefrance <kirmaxmel@gmail.com>
2021-02-24 16:30:59 +0000

    5th commit - selenide 5.19 - test passed

--- src/test/java/tests/DragAndDrop.java
+++ src/test/java/tests/DragAndDrop.java
@@ -23,10 +23,11 @@ public class DragAndDrop {
     open("https://the-internet.herokuapp.com/drag_and_drop");
     $("#column-a").shouldHave(text("A"));
     $("#column-b").shouldHave(text("B"));
-    sleep(3000);
-    actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200, 0).release().perform();
-    // $("#column-a").dragAndDropTo("#column-b"); не работает
+    sleep(1000);
+    $("#column-a").dragAndDropTo("#column-b");
     $("#column-a").shouldHave(text("B"));
     $("#column-b").shouldHave(text("A"));
+    sleep(1000);
+    // sleep - для наглядности
     }
 }
